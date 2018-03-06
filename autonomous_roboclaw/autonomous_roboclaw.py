aa7281f9f1c13d28a11ed16c2e73851dd014f6a3
lt <theurerlukas@gmail.com>
2018-03-06 16:50:03 +0100

    cleanup and stops wheel at exit

--- autonomous_roboclaw/autonomous_roboclaw.py
+++ autonomous_roboclaw/autonomous_roboclaw.py
@@ -1,34 +1,22 @@
 import serial
-import time, math
-import sys, tty, termios
+import time
 import Adafruit_PCA9685
 import os
-
-import VL53L0X
+import autonomous_roboclaw.VL53L0X as VL53L0X
+import atexit
 
 #Initialization at address 0x40
 pwm = Adafruit_PCA9685.PCA9685()
 
-servoMin = 200  # Min pulse length out of 4096
-servoMax = 300  # Max pulse length out of 4096
+SERVO_MIN = 200  # Min pulse length out of 4096
+SERVO_MAX = 300  # Max pulse length out of 4096
 
-
-class _Getch:
-    def __call__(self):
-        fd = sys.stdin.fileno()
-        old_settings = termios.tcgetattr(fd)
-        try:
-            tty.setraw(sys.stdin.fileno())
-            ch = sys.stdin.read(3)
-        finally:
-            termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
-        return ch
-
-
+def stop_at_exit(c):
+    c.stop_all_wheels()
 class control:
     global pwm
     speed_right_wheels = 0
-    speedM2 = 0
+    speed_left_wheels = 0
 
     def __init__(self):
         pass
@@ -71,112 +59,6 @@ class control:
         self.move_right_wheels_backward(x)
         self.move_left_wheels_backward(x)
 
-    def test(self):
-        self.move_all_wheels_forward(40)
-        time.sleep(3)
-        self.stop_all_wheels()
-        time.sleep(1)
-        self.move_all_wheels_backward(40)
-        time.sleep(3)
-        self.stop_all_wheels()
-
-    def up(self):
-        if self.speed_right_wheels != self.speedM2:
-            self.speed_right_wheels = ((self.speed_right_wheels + self.speedM2) / 2) - (((self.speed_right_wheels + self.speedM2) / 2) % 10)
-            self.speedM2 = self.speed_right_wheels
-        if self.speed_right_wheels >= 0 or self.speedM2 >= 0:
-            self.speed_right_wheels += 10
-            self.speedM2 += 10
-            if self.speed_right_wheels > 120:
-                self.speed_right_wheels = 120
-            if self.speedM2 > 120:
-                self.speedM2 = 120
-            self.move_right_wheels_forward(self.speed_right_wheels)
-            self.move_left_wheels_forward(self.speedM2)
-
-        elif self.speed_right_wheels < 0 or self.speedM2 < 0:
-            self.speed_right_wheels += 10
-            self.speedM2 += 10
-            if self.speed_right_wheels > 120:
-                self.speed_right_wheels = 120
-            if self.speedM2 > 120:
-                self.speedM2 = 120
-            self.move_right_wheels_backward(abs(self.speed_right_wheels))
-            self.move_left_wheels_backward(abs(self.speedM2))
-
-    #		if self.speedM1 == 0:
-    #			self.stop()
-
-    def down(self):
-        if self.speed_right_wheels != self.speedM2:
-            self.speed_right_wheels = ((self.speed_right_wheels + self.speedM2) / 2) - (((self.speed_right_wheels + self.speedM2) / 2) % 10)
-            self.speedM2 = self.speed_right_wheels
-        if self.speed_right_wheels > 0 or self.speedM2 > 0:
-            self.speed_right_wheels -= 10
-            self.speedM2 -= 10
-            if self.speed_right_wheels > 120:
-                self.speed_right_wheels = 120
-            if self.speedM2 > 120:
-                self.speedM2 = 120
-            self.move_right_wheels_forward(self.speed_right_wheels)
-            self.move_left_wheels_forward(self.speedM2)
-
-        elif self.speed_right_wheels <= 0 or self.speedM2 <= 0:
-            self.speed_right_wheels -= 10
-            self.speedM2 -= 10
-            if self.speed_right_wheels < -120:
-                self.speed_right_wheels = -120
-            if self.speedM2 < -120:
-                self.speedM2 = -120
-            self.move_right_wheels_backward(abs(self.speed_right_wheels))
-            self.move_left_wheels_backward(abs(self.speedM2))
-
-    #		if self.speedM1 == 0:
-    #			self.stop()
-
-    def left(self):
-        if self.speed_right_wheels > 0 or self.speedM2 > 0:
-            self.speed_right_wheels += 10
-            self.speedM2 -= 10
-            if self.speed_right_wheels > 120:
-                self.speed_right_wheels = 120
-            if self.speedM2 <= 0:
-                self.speedM2 = 0
-            self.move_right_wheels_forward(self.speed_right_wheels)
-            self.move_left_wheels_forward(self.speedM2)
-
-        elif self.speed_right_wheels < 0 or self.speedM2 < 0:
-            self.speed_right_wheels += 10
-            self.speedM2 -= 10
-            if self.speed_right_wheels > 0:
-                self.speed_right_wheels = 0
-            if self.speedM2 > -120:
-                self.speedM2 = -120
-            self.move_right_wheels_backward(abs(self.speed_right_wheels))
-            self.move_left_wheels_backward(abs(self.speedM2))
-
-    def right(self):
-        if self.speed_right_wheels > 0 or self.speedM2 > 0:
-            self.speedM2 += 10
-            self.speed_right_wheels -= 10
-            if self.speedM2 > 120:
-                self.speedM2 = 120
-            if self.speed_right_wheels <= 0:
-                self.speed_right_wheels = 0
-            self.move_right_wheels_forward(self.speed_right_wheels)
-            self.move_left_wheels_forward(self.speedM2)
-
-        elif self.speed_right_wheels < 0 or self.speedM2 < 0:
-            self.speedM2 += 10
-            self.speed_right_wheels -= 10
-            if self.speedM2 > 0:
-                self.speedM2 = 0
-            if self.speed_right_wheels < -120:
-                self.speed_right_wheels = -120
-            self.move_right_wheels_backward(abs(self.speed_right_wheels))
-            self.move_left_wheels_backward(abs(self.speedM2))
-
-
 def main():
     global pwm
     tof = VL53L0X.VL53L0X()
@@ -186,6 +68,7 @@ def main():
     print(os.path.dirname(os.path.abspath(__file__)))
 
     c = control()
+    atexit.register(stop_at_exit, c)
     # c.test()
     # print 'Enter commands:'
     c.up()
@@ -202,7 +85,7 @@ def main():
     print("Timing %d ms" % (timing / 1000))
 
     while (1):
-        pwm.set_pwm(0, 0, servoMin)
+        pwm.set_pwm(0, 0, SERVO_MIN)
         time.sleep(0.5)
         distance = tof.get_distance()
         print("1: %d mm, %d cm" % (distance, (distance / 10)))
@@ -215,7 +98,7 @@ def main():
             stop1 = 0
             print("Frei_1!")
         # time.sleep(0.5)
-        pwm.set_pwm(0, 0, servoMax)
+        pwm.set_pwm(0, 0, SERVO_MAX)
         time.sleep(0.5)
         distance = tof.get_distance()
         print("2: %d mm, %d cm" % (distance, (distance / 10)))
