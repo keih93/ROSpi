10ea65447a4cc0ea7cb05ae653b2f09e60c1138b
lt <theurerlukas@gmail.com>
2018-03-07 09:51:09 +0100

    Removed engine logic

--- autonomous_roboclaw/autonomous_roboclaw.py
+++ autonomous_roboclaw/autonomous_roboclaw.py
@@ -1,10 +1,10 @@
 from time import sleep
 
-import serial
+import autonomous_roboclaw.Engine as Engine
 import time
 import Adafruit_PCA9685
 import os
-import VL53L0X as VL53L0X
+import autonomous_roboclaw.VL53L0X as VL53L0X
 import atexit
 
 # Initialization at address 0x40
@@ -14,56 +14,8 @@ SERVO_MIN = 200  # Min pulse length out of 4096
 SERVO_MAX = 300  # Max pulse length out of 4096
 
 
-def stop_at_exit(c):
-    c.stop_all_wheels()
-
-
-class control:
-    global pwm
-    speed_right_wheels = 0
-    speed_left_wheels = 0
-
-    def __init__(self):
-        pass
-
-    ser = serial.Serial(
-        port='/dev/ttyUSB0',
-        baudrate=19200,
-        parity=serial.PARITY_NONE,
-        stopbits=serial.STOPBITS_ONE,
-        bytesize=serial.EIGHTBITS
-    )
-
-    def move_right_wheels_forward(self, x):
-        self.ser.write(bytes([128, 0, x, (128 + x) & 0x7F]))
-
-    def move_right_wheels_backward(self, x):
-        self.ser.write(bytes([128, 1, x, (129 + x) & 0x7F]))
-
-    def stop_right_wheels(self):
-        self.ser.write(bytes([128, 0, 0, 128 & 0x7F]))
-
-    def move_left_wheels_backward(self, x):
-        self.ser.write(bytes([128, 4, x, (132 + x) & 0x7F]))
-
-    def move_left_wheels_forward(self, x):
-        self.ser.write(bytes([128, 5, x, (133 + x) & 0x7F]))
-
-    def stop_wheels_left(self):
-        self.ser.write(bytes([128, 4, 0, 132 & 0x7F]))
-
-    def move_all_wheels_forward(self, x):
-        self.move_right_wheels_forward(x)
-        self.move_left_wheels_forward(x)
-
-    def stop_all_wheels(self):
-        self.stop_right_wheels()
-        self.stop_wheels_left()
-
-    def move_all_wheels_backward(self, x):
-        self.move_right_wheels_backward(x)
-        self.move_left_wheels_backward(x)
-
+def stop_at_exit(engine):
+    engine.stop_all_wheels()
 
 def main():
     global pwm
@@ -73,13 +25,13 @@ def main():
     tof.start_ranging(VL53L0X.VL53L0X_BETTER_ACCURACY_MODE)
     print(os.path.dirname(os.path.abspath(__file__)))
 
-    c = control()
-    atexit.register(stop_at_exit, c)
-    c.move_all_wheels_forward(20)
+    engine = Engine.Engine()
+    atexit.register(stop_at_exit, engine)
+    engine.move_all_wheels_forward(20)
     sleep(2)
-    c.move_all_wheels_backward(20)
+    engine.move_all_wheels_backward(20)
     sleep(2)
-    c.stop_all_wheels()
+    engine.stop_all_wheels()
 
 
     stop1 = 0
@@ -96,7 +48,7 @@ def main():
         distance = tof.get_distance()
         print("1: %d mm, %d cm" % (distance, (distance / 10)))
         if (distance > 200):
-            c.stop_all_wheels()
+            engine.stop_all_wheels()
             stop1 = 1
             print("Stopp_1!")
         else:
@@ -109,7 +61,7 @@ def main():
         distance = tof.get_distance()
         print("2: %d mm, %d cm" % (distance, (distance / 10)))
         if (distance < 200):
-            c.stop_all_wheels()
+            engine.stop_all_wheels()
             stop2 = 1
             print("Stopp_2!")
         else:
@@ -117,7 +69,7 @@ def main():
             stop2 = 0
             print("Frei_2!")
         if (stop1 == 0 and stop2 == 0):
-            c.move_all_wheels_forward(20)
+            engine.move_all_wheels_forward(20)
 
 
 if __name__ == '__main__':
