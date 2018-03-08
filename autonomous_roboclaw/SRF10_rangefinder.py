2f8b0abd627f08828e55bc90f6841eb2325f3ea6
lt <theurerlukas@gmail.com>
2018-03-08 15:24:35 +0100

    Added print in RSF

--- autonomous_roboclaw/SRF10_rangefinder.py
+++ autonomous_roboclaw/SRF10_rangefinder.py
@@ -149,7 +149,7 @@ class SRFBase(object):
         # data is pack in big-endian form
         for i in range(2, len(self.rxb), 2):
             range_val = (self.rxb[i - 1] << 8) + self.rxb[i + 1]
-            print("Abstand SRF10 in cm : " + range_val)
+            print("Abstand SRF10 in cm : {}".format(range_val))
             if range_val > 0:
                 values.append(range_val)
         values.append(self.rxb[0])
