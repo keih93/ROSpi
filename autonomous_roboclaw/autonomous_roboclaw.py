c65e6159bce7f27a9bdb5ae16f422d1ca1365c37
lt <theurerlukas@gmail.com>
2018-03-08 09:38:42 +0100

    Measure range

--- autonomous_roboclaw/autonomous_roboclaw.py
+++ autonomous_roboclaw/autonomous_roboclaw.py
@@ -36,7 +36,8 @@ def main():
     while (1):
         print("Sensor left distance in mm: " + str(sensors.tof_left.get_distance()))
         print("Sensor right distance in mm: " + str(sensors.tof_right.get_distance()))
-        print("USS read_range : " + str(rf.read_range()))
+        value = rf.measure_and_read()
+        print("USS read_range : " + str(value))
         time.sleep(2)
 
     stop1 = 0
