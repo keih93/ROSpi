4271d6e31b74935eca8b70e6c232f8eba00ef38b
gerdablum <jaal1013@hs-karlsruhe.de>
2019-03-15 10:26:07 +0100

    Make tail moving  work

--- autonomous_roboclaw/tracking_module.py
+++ autonomous_roboclaw/tracking_module.py
@@ -2,6 +2,9 @@ import camera_module
 Position = camera_module.Position
 import Servos
 import math
+import time
+from threading import Thread
+
 
 class TrackingModule:
     
@@ -12,16 +15,16 @@ class TrackingModule:
     servoTail = s.servoTail
     lastSeenX = None
     lastSeenY = None
-    tailForward = True
+    moveTailForward = True
     
     def __init__(self):
         pass
-
+    
     def followObject(self):
         def calcSpeeds():
-            """Helper subroutine"""
+            """Helper subroutine, using outer vars x, y, x_speed, y_speed"""
             n_x = abs(x_speed - 0.5)
-            n_x *= math.log(1 + n_x)  #abs(x_speed - 0.5)**2  # TODO simple x^2-function is not ideal here!
+            n_x *= math.log(1 + n_x) 
             
             if x is Position.TOO_HIGH:
                 x_s = n_x
@@ -41,28 +44,32 @@ class TrackingModule:
                 y_s = 0
             return x_s, y_s
         
-        x_step = 200
-        y_step = 200
+        x_step = 100
+        y_step = 40
         
         currentOffset = self.camera.getPosition() 
         currentSector = self.camera.getPositionOfObject(currentOffset)
         
-        if currentSector is None or currentOffset is None:
-            if self.lastSeenX is None or self.lastSeenY is None:
-                # TODO go into default pose, or start scanning the surrounding
+        if currentSector is None or currentOffset is None:  # if no object found
+            if self.lastSeenX is None or self.lastSeenY is None:  # and we have never seen it
+                # go into default pose? start scanning the surrounding?
                 return
-            x = self.lastSeenX
-            y = self.lastSeenY
-            x_speed = (float) (x.value / 10)  # search in last direction with offset +-1!
-            y_speed = (float) (y.value / 10) # XXX using knowledge about enum values ...
+            else:
+                # look in the direction, we have seen the object lately
+                x = self.lastSeenX
+                y = self.lastSeenY
+                x_speed = float(x.value / 10.0)  # search in last direction with offset +-1!
+                y_speed = float(y.value / 10.0) # XXX using knowledge about enum values ...
+                
         else:
-            x, y = currentSector
-            x_speed, y_speed = currentOffset
-            self.lastSeenX, self.lastSeenY = currentSector # save for later
+            # object found
             self.moveTail()
-        
+            x, y = currentSector
+            x_speed, y_speed = currentOffset  # temporary, used in calcSpeeds()
+            self.lastSeenX, self.lastSeenY = currentSector # save for later
+            
         if x is Position.CENTER and y is Position.CENTER:
-            # nothing to do, we are looking at the object
+            # nothing to do, we are looking at the object!
             return
         
         x_speed, y_speed = calcSpeeds()
@@ -70,20 +77,25 @@ class TrackingModule:
         self.servoFace.addval(x_speed * x_step)
         
         self.servoHead.addval(y_speed * y_step)
-
+    
     def moveTail(self):
-        if self.tailForward:
+        if self.moveTailForward:
             self.servoTail.addval(20)
-            self.tailForward = False
+            if self.servoTail.val >= self.servoTail.max_val:
+                self.moveTailForward = False
         else:
             self.servoTail.addval(-20)
-            self.tailForward = True
-        
+            if self.servoTail.val <= self.servoTail.min_val:
+                self.moveTailForward = True
+
+         
+            
             
 def main():
     tm = TrackingModule()
     while True:
         tm.followObject()
+        #tm.moveTail()
 
 if __name__ == '__main__':
     main()
